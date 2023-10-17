40753396eac3c88f53c54647d6b61d0fce1cba7e
Fernando Alberto Villafaña <ffernando.villafanaalfonseca@gmail.com>
2023-10-16 19:22:04 -0500

    Login con google

--- frontend_gice/src/pages/Login/Login.js
+++ frontend_gice/src/pages/Login/Login.js
@@ -1,45 +1,70 @@
-import React, {useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import Navbar from '../../components/Navbar/Navbar';
 import Footer from '../../components/Footer/Footer';
 import "./Login.css"
+import jwt_decode from 'jwt-decode'
 
 const Login = () => {
+
+    function handleCallbackResponse(response) {
+        console.log('Encoded JWT ID token: ' + response.credential)
+        var userObject = jwt_decode(response.credential)
+        console.log(jwt_decode(response.credential))
+    }
+
+    useEffect(() => {
+        /* global google*/
+        google.accounts.id.initialize({
+            client_id: '990331222556-dv2o8bfq96mcir6ac91ohene0qg34v69.apps.googleusercontent.com',
+            callback: handleCallbackResponse
+        })
+
+        google.accounts.id.renderButton(
+            document.getElementById('signInDiv'),
+            { size: 'large', shape:'pill' }
+        )
+
+    }, [])
+
     const [show, setShow] = useState(false);
     return (
         <div className='Login'>
             <div className='imageContainer'></div>
-            <Navbar show={show} setShow={setShow}/>
-            {show?
-            null:
-            <div className='loginContainer'>
-                
-                <div className='titleContainer'>
-                
-                    
-                    <h1 className='titleLogin'>Iniciar Sesión</h1>
-                </div>
-                <div className='loginFormContainer'>
-                    
-                    <form className='loginForm'>
-                        {/* <label>Usuario</label> */}
-                        <input type='text' placeholder='CORREO' className='inputLogin'/>
-                        {/* <label>Contraseña</label> */}
-                        <input type='password' placeholder='CONTRASEÑA' className='inputLogin'/>
-                        <a href='' className='forgotPasswordLink'>OLVIDE MI CONTRASEÑA</a>
-                        <button type='submit' className='buttonLogin'>Iniciar Sesión</button>
-                        <button type='submit' className='buttonLogin'>Iniciar Sesión con Google</button>
-                        <button type='submit' className='buttonRegister'>REGISTRARSE</button>
-                    </form>
+            <Navbar show={show} setShow={setShow} />
+            {show ?
+                null :
+                <div className='loginContainer'>
 
-                </div>
-            </div>}
+                    <div className='titleContainer'>
+
+
+                        <h1 className='titleLogin'>Iniciar Sesión</h1>
+                    </div>
+                    <div className='loginFormContainer'>
+
+                        <form className='loginForm'>
+                            {/* <label>Usuario</label> */}
+                            <input type='text' placeholder='CORREO' className='inputLogin' />
+                            {/* <label>Contraseña</label> */}
+                            <input type='password' placeholder='CONTRASEÑA' className='inputLogin' />
+                            <a href='' className='forgotPasswordLink'>OLVIDE MI CONTRASEÑA</a>
+                            <button type='submit' className='buttonLogin'>Iniciar Sesión</button>
+                            <div style={{display:'flex', height:70+ 'px', alignItems:'center', justifyContent:'center'}}>
+                                <div id='signInDiv'></div>
+                            </div>
+                            <button type='submit' className='buttonRegister'>REGISTRARSE</button>
+                            
+                        </form>
+
+                    </div>
+                </div>}
 
             {
-            show?
-            null:
-            <Footer/>
+                show ?
+                    null :
+                    <Footer />
             }
-            
+
         </div>
     );
 }
