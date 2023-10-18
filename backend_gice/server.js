5c13068a48689a35a5ddbc51e42d3c0852ba9cb3
kevin-alexis <kevinlevin367@outlook.com>
2023-10-17 23:10:35 -0500

    Add Script to create dir uploads

--- backend_gice/server.js
+++ backend_gice/server.js
@@ -1,8 +1,19 @@
 import app from './app.js';
 const port = 8080;
+import fs from 'fs';
+
+// Verificar si la carpeta uploads existe, si no, crearla
+const uploadsFolder = './uploads';
+if (!fs.existsSync(uploadsFolder)) {
+  fs.mkdirSync(uploadsFolder);
+
+console.log('Carpeta uploads creada.');
+}
 
 // Iniciar el servidor, escucha en el puerto 8080
 app.listen(port, () => {
     console.log(`Servidor iniciado en el puerto ${port}`);
 })
+
+
 
